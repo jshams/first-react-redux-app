8fda0d575af32ef0d78c666aedad62f1aa2ae6a2
Jake Shams <jshams62@gmail.com>
2019-05-01 16:02:33 -0700

    added reset switch case

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,7 +1,7 @@
 // responsible for handling increment and decrement situations
-import { INCREMENT, DECREMENT } from '../actions'
+import { INCREMENT, DECREMENT, RESET } from '../actions'
 
-const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 50, action) => {
     // two parameters:
     //  state is the current state
     //  action is the action we are calling on the state
@@ -10,6 +10,8 @@ const counterReducer = (state = 0, action) => {
             return state += 1
         case DECREMENT:
             return state -= 1
+        case RESET:
+            return 50
         default:
             return state
     }
