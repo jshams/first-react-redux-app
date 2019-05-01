8ae984b3ea6b571826b6cdfd35c83ec5c3ac73b0
Jake Shams <jshams62@gmail.com>
2019-05-01 16:02:00 -0700

    added reset function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 const INCREMENT = 'INCREMENT'
 const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
 
 const increment = () => ({ type: INCREMENT })
 const decrement = () => ({ type: DECREMENT })
+const reset = () => ({ type: RESET })
 
-export { INCREMENT, DECREMENT, increment, decrement }
+export { INCREMENT, DECREMENT, RESET, increment, decrement, reset }
