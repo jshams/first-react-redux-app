5eaca12371682833191e3a3d42c11c56e6703e6b
Jake Shams <jshams62@gmail.com>
2019-05-02 16:13:31 -0700

    added reset button

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,14 @@
 import React from 'react'
-import { increment, decrement, reset } from './actions'
-import { connect } from 'react-redux'
 
 const Counter = (props) => {
     return (
         <div>
             <h1>{props.count}</h1>
-            <button
-                onClick={() => props.increment(5)}
-            >up</button>
-            <button
-                onClick={() => props.decrement(5)}
-            >down</button>
-            <button
-                onClick={() => props.reset(0)}
-            >reset</button>
+            <button onClick={() => props.increment()}>up</button>
+            <button onClick={() => props.decrement()}>down</button>
+            <button onClick={() => props.reset()}>reset</button>
         </div >
     )
 }
 
-const mapStateToProps = (state) => {
-    return { count: state.counter }
-}
-
-const mapStateToDispatch = () => {
-    return {
-        increment,
-        decrement,
-        reset
-    }
-}
-
-export default connect(mapStateToProps, mapStateToDispatch())(Counter)
+export default Counter
